23993ba078279bd39e3d7dfeb8597d16f208df1b
IliesGuellaf <108081986+IluHeart@users.noreply.github.com>
2023-06-27 10:41:57 +0200

    S

---
+++ team.js
@@ -0,0 +1,28 @@
+const teamMemmbers = [
+  {
+    name: "Ilies",
+    surname: "Guellaf",
+    age: 21,
+    city: "Los Montesinos",
+    hobby: "Dibujar",
+    favoriteFood: "Ensaladilla rusa",
+    favoriteVideoGame: "Pokemon HeartGold",
+    favoriteFilm: "Blade Runner 2049",
+    favoriteBook: "Manga:PunPun",
+    petName: "",
+  },
+  {
+    name: "Wilmer",
+    surname: "Salazar",
+    age: 21,
+    city: "Los Montesinos",
+    hobby: "Dibujar",
+    favoriteFood: "Ensaladilla rusa",
+    favoriteVideoGame: "Pokemon HeartGold",
+    favoriteFilm: "Blade Runner 2049",
+    favoriteBook: "Manga:PunPun",
+    petName: "",
+  },
+];
+
+
