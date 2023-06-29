842e11f13d20c90d58483480103eb5af317c4a4a
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-06-29 23:25:46 +0200

    Subiendo mi parte del ejercicio

--- team.js
+++ team.js
@@ -23,6 +23,50 @@ const teamMemmbers = [
     favoriteBook: "El principito",
     petName: "",
   },
+  {
+    name: "Nicolae Alexandru",
+    surname: "Casiean",
+    age: 25,
+    city: "Valencia",
+    hobby: "Gaming",
+    favoriteFood: "Hamburger",
+    favoriteVideoGame: "Valorant",
+    favoriteFilm: "Coco",
+    favoriteBook: "Harry Potter el misterio de azkaban",
+    petName: "Dog: rocky. Cat:Della",
+  },
+  {
+    name: "Anabel",
+    surname: "Castillo",
+    age: 27,
+    city: "Cádiz",
+    hobby: "Crossfit",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Kingdom Hearts",
+    favoriteFilm: "Pesadilla antes de Navidad",
+    favoriteBook: "Canción de Hielo y Fuego",
+    petName: "",
+  },
+
 ];
+
+  // Print the team in age order (name age). Wilmer
+
+  teamMemmbers.sort((a, b) => a.age - b.age);
+
+  teamMemmbers.forEach(member => {
+    console.log(`${member.name} ${member.age}`);
+  });
+
+
+
+//Print the team middle age. Wilmer
+
+const sumOfAges = teamMemmbers.reduce((sum, member) => sum + member.age, 0);
+
+
+const averageAge = sumOfAges / teamMemmbers.length;
+
+console.log("La edad promedio del equipo es: " + averageAge);
 
 
